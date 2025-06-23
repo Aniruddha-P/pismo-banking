cd126ce4cfc0344f411cc21a3b5c4e0a22aef861
Aniruddha-P <pardhi.aniruddha@gmail.com>
2025-06-23 07:48:29 +0530

    Removed unnecessary code in GlobalExceptionHandler and optimised imports

--- src/main/java/com/pismo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/pismo/exceptions/GlobalExceptionHandler.java
@@ -6,15 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Hidden
 @RestControllerAdvice
@@ -22,15 +21,7 @@ public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        List<String> errors = ex.getBindingResult().getFieldErrors()
-                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-    }
-
-  /*  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new LinkedHashMap<>();
 
@@ -42,7 +33,7 @@ public class GlobalExceptionHandler {
         });
 
         return ResponseEntity.badRequest().body(errors);
-    }*/
+    }
 
     @ExceptionHandler({AccountNotFoundException.class, InappropriateAmountException.class, OperationNotFoundException.class,
             TransactionNotFoundException.class, AccountIdNotProvidedException.class})
